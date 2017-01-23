08708196298cc428bf8bd3c9f8e4bceb20a93987
abhiagr <abhi.iitr@gmail.com>
2017-01-22 18:34:22 -0800

    Publishing code to public repo.

---
+++ README.md
@@ -0,0 +1,24 @@
+Open/Launch in Visual Studio
+=============
+
+>The extension enables you to open a file in Visual Studio IDE if it is installed in your system with great ease.
+
+>Just press F1 and type "Open in VS" or "Launch in VS" and the file will be opened in an existing session of Visual Studio if  Visual Studio is already running else it will launch a new session of  Visual Studio. 
+
+##Keyboard Shortcut
+>You can also use Alt+shift+V to open the file in Visual Studio. 
+
+##Compatibility with different versions of visual Studio
+__The extension is tested for Visual Studio 2015, but it has been written to work for 2010, 2012 and 2013 versions also. Please mail me if it doesn't work for you.__  
+
+
+##Feedback
+>Please dont forget to __Rate__ this extension. Please give your comments also.
+
+
+##How to update 
+>Launch the VS Code Command Pallete - Ctrl+P - and type the command  __ext update livs__ 
+
+>For more info visit https://code.visualstudio.com/Docs/editor/extension-gallery#_update-an-extension
+
+
---
+++ extension.js
@@ -0,0 +1,101 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+var vscode = require('vscode');
+
+function doesExist(path) {
+    var fs = require('fs');
+
+    try {
+        fs.accessSync(path, fs.F_OK);
+        return true;
+    } catch (e) {
+        return false;
+    }
+}
+
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+function activate(context) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "launch-in-visual-studio" is now active!'); 
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with  registerCommand
+	// The commandId parameter must match the command field in package.json
+	var disposable = vscode.commands.registerCommand('extension.launchInVS', function () {
+   
+        var vsPath2015 = 'C:\\Program Files (x86)\\Microsoft Visual Studio 14.0\\Common7\\IDE\\devenv.exe';
+        var vsPath2013 = 'C:\\Program Files (x86)\\Microsoft Visual Studio 12.0\\Common7\\IDE\\devenv.exe';
+        var vsPath2012 = 'C:\\Program Files (x86)\\Microsoft Visual Studio 11.0\\Common7\\IDE\\devenv.exe';
+        var vsPath2010 = 'C:\\Program Files (x86)\\Microsoft Visual Studio 10.0\\Common7\\IDE\\devenv.exe';
+        //var vsPath1 = 'C:\\Program Files (x86)\\Microsoft Visual Studio 14.0\\Common7\\IDE\\devenv1.exe';
+        
+        
+        var vsPath;
+        
+        if (doesExist(vsPath2015))
+        {
+            vsPath = vsPath2015;            
+        }
+        else if (doesExist(vsPath2013))
+        {
+            vsPath = vsPath2013;            
+        }
+        else if (doesExist(vsPath2012))
+        {
+            vsPath = vsPath2012;            
+        }
+        else if (doesExist(vsPath2010))
+        {
+            vsPath = vsPath2010;            
+        }
+        else 
+        {
+            vscode.window.showErrorMessage('Visual Studio not available on this machine.');
+            return;
+        }
+        
+        
+        var editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            vscode.window.showErrorMessage('Could not find and valid text editor. Please open a file in Code first.');
+            return; // No open text editor
+        }
+        
+        if (editor.document.isUntitled) {
+            vscode.window.showErrorMessage('Please save the file first.');
+            return; // No open text editor
+        }
+        
+        var myfilename = editor.document.fileName;
+         
+        // Display a message box to the user
+        //vscode.window.showInformationMessage('Will open the file ' + text + 'in Visual Studio');
+        
+        // The code you place here will be executed every time your command is executed
+        const execFile = require('child_process').execFile;
+        const bat = execFile(vsPath,
+            ['/edit', myfilename], (error, stdout, stderr) => {
+                console.log(`stdout: ${stdout}`);
+                console.log(`stderr: ${stderr}`);
+                 if (error !== null) {
+                     console.log('Some error occured while launching Visual Studio.');
+                 }
+                
+            });
+
+        // Display a message box to the user
+        console.log('Opened file in VS!');
+	});
+	
+	context.subscriptions.push(disposable);
+}
+exports.activate = activate;
+
+// this method is called when your extension is deactivated
+function deactivate() {
+}
+exports.deactivate = deactivate;
